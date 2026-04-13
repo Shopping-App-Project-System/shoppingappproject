0350748f011088dda4b0b09cc9719259d61f63fe
Shopping-App-Project-System <shoppingappprojectsystem@gmail.com>
2026-04-13 09:11:12 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,10 @@
 # DATABASE = ...
 
 # ________________________FLASK_________________________
-# APP_PORT = ...
+APP_PORT = 8000 # FLASK啟用時所用的埠號 預設為8000 可以自行調整
+SESSION_KEY = "MyShoppingAppProject" # 建立SESSION時使用的SESSION KEY
+SESSION_AUTHO = "AUTHO" # 用於儲存使用者登入狀態的KEY 各分支會將以此作為判斷使用者是否登入用的依據(整合後才會用到)
+
 
 
 """
