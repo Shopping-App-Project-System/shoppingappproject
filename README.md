9de17f9424f1dffeea2d48a241e8d101038cb69b
cbes502034 <cbes502034@gmail.com>
2026-04-17 11:05:57 +0800

    Upload improvement strategy to README.md 1

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ branchC table columns :
     order_items(id, order_id, product_id, quantity, price)
 
 branchD table columns :
+
+______________________________________________________
+branchA 改善策略: 
+    1.資料表無法寫入中文字
+    2.token的防呆過強，已經嚴重影響使用上的體驗(只要一次重新整理頁面，驗證信就報銷了)
+    3.token尚未設定過期時間
+    4.驗證信發送的等待時間尚未設置
+    5.防止駭客的密碼錯誤次數機制可考慮加入
+    6.google authenticator totp驗證碼機制，可視情況斟酌添加
+    7.輸入的資料尚未設計正則過篩機制
+    8.email驗證信件內容缺乏美觀設計
+    9.controler內的程式碼，可進行物件導向設計
+    10.各套件需分門別類，並未後續整合後，大家的模組有地方可以歸納
--- utils.py
+++ utils.py
@@ -32,27 +32,4 @@ def checkUserInput(datas, **msgs):
 
 def getRandomVerifyCode(digits):return "".join(list(str(randint(0,9)) for _ in range(digits)))
 
-def getVerifyToken(digits):return token_urlsafe(digits)
-
-# def getProfilePicture(data):
-#     file = data.get("profile_pic")
-
-#     if not file or file.filename == "":
-#         return None, "default.jpg"
-    
-#     extension = file.filename.rsplit(".",1)[-1]
-#     filename = settings.PROFILE_TEMP_PATH.format()
-#     # path = os.path.join(settings.PROFILE_TEMP_PATH, filename)
-#     return file, path
-# class ProfilePic:
-#     def setProfilePicData(self,data,name):
-        
-        
-#     def setExtension(self,extension):
-#         self.extension = extension
-    
-        
-# def buildFileName(account,temp=False):
-#     if temp:
-#         file_name = settings.PROFILE_PIC_PATH
-    
+def getVerifyToken(digits):return token_urlsafe(digits)    
