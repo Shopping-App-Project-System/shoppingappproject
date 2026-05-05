7bed44fca0eed8d0cd64a17da39b041fd54d3b0f
cbes502034 <cbes502034@gmail.com>
2026-05-05 15:08:57 +0800

    所有驗證裝飾器邏輯修正完畢 可正常運作 並增加新的驗證->有登入非登入皆可使用 只有admin除外

--- modules/Product/Product_routers.py
+++ modules/Product/Product_routers.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 
 # _______________________________________自定義模組_______________________________________
 from .Product_services import index_service,product_detail_service
-from AuthDecorator import userRequired
+from AuthDecorator import blockAdmin
 
 # _______________________________________初始化___________________________________________
 bp = Blueprint("B",__name__)
@@ -11,11 +11,11 @@ bp = Blueprint("B",__name__)
 # ________________________________________API_____________________________________________
 
 @bp.route("/")
-@userRequired
+@blockAdmin
 def index():return index_service()
 
 # 商品詳細頁
-@userRequired
-@bp.route("/index/product/<int:product_id>")
+@bp.route("/product/<int:product_id>")
+@blockAdmin
 def product_detail(product_id):return product_detail_service(product_id)
 
