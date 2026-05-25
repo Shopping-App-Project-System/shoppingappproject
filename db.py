5dda44debb69b44e0bf2533f156225715c459735
cbes502034 <cbes502034@gmail.com>
2026-05-25 10:14:12 +0800

    make it better

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 from settings import DB_HOST,DB_PORT,DB_USER,DB_PASSWORD,DB_DATABASE
-import mariadb
+import pymysql
 from functools import wraps
 # __ 資料庫操作_________________________________________________________________
 def db_transaction(fun):
     @wraps(fun)
     def wrap(*args,**kwargs):
-        conn = mariadb.connect(host=DB_HOST, port=DB_PORT,
+        conn = pymysql.connect(host=DB_HOST, port=DB_PORT,
                 user=DB_USER, password=DB_PASSWORD,
                 database=DB_DATABASE)
         cursor = conn.cursor(dictionary=True)
